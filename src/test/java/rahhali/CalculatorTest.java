22f56d580491ae33fd09d26c3198004b470ceb26
jrahhali <rahhali@live.com>
2015-07-08 07:44:07 -0300

    Trying to figure out the best way to get gradle dependencies on the eclipse classpath

--- src/test/java/rahhali/CalculatorTest.java
+++ src/test/java/rahhali/CalculatorTest.java
@@ -1,5 +1,13 @@
 package rahhali;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculatorTest {
 
+	@Test
+	public void testAdd() {
+		assertEquals("Hello World", "Hello World");
+	}
 }
